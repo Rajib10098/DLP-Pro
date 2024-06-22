0b2a8e96f275b16423e9239c2efcd9e731b9ef6d
Rajib10098 <82735278+Rajib10098@users.noreply.github.com>
2024-06-22 20:47:11 +0530

    Testing

--- dlp-pro.sh
+++ dlp-pro.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
 fi
 
 filename="$1"
-location=~/AKI
+location=~/DLP-Pro
 
 if [ ! -d "$location" ]; then
     # If it doesn't exist, create it
@@ -27,41 +27,48 @@ fi
 # album_image=$(cat $filename | grep -oP '(?<=album_image=").*?(?=")')
 album_image=$(grep -m 1 -oP '(?<=album_image=").*?(?=")' "$filename")
 album_name=$(grep -m 1 -oP '(?<=album=").*?(?=")' "$filename")
-wget -O "$location/$album_name.jpg" "$album_image" > /dev/null 2>&1
 
+# Download Song art-cover
+wget -O "$location/$album_name.jpg" "$album_image" > /dev/null 2>&1
+# Print output in console
 echo "Downloaded successfully: Album cover art"
 echo "----------------------------------------"
 
 
 # Loop over each line in the file
 while IFS= read -r line; do
+    
+    # Varrible inside loop
     index=$(echo "$line" | grep -oP '(?<=song_index=)\d+')
-    # album=$(echo "$line" | grep -oP '(?<=album=").*?(?=")')
-    # album_img=$(echo "$line" | grep -oP '(?<=album_image=").*?(?=")')
     name=$(echo "$line" | grep -oP '(?<=song_name=").*?(?=")')
     song_url=$(echo "$line" | grep -oP '(?<=song_link=").*?(?=")')
     artist_name=$(echo "$line" | grep -oP '(?<=artist_name=").*?(?=")')
     year=$(echo "$line" | grep -oP '(?<=year=")\d*?(?=")')
     
-    # echo "Song: $index"
-    # echo "album: $album_name"
-    # echo "Album image: $album_image"
-    # echo "Song: $name"
-    # echo "Song url: $song_url"
-    # echo $location
+    
     
     cd "$location"
+    
+    # Download the song with YouTube-dlp
     song_file_name=$( yt-dlp -x -f 'bestaudio[ext=m4a]' --add-metadata --embed-thumbnail $song_url | grep -oP '(?<=Adding metadata to ").+\.m4a' )
-    # echo "Song file name: $song_file_name"
+    
+    # Remove song's art-cover
     AtomicParsley "$song_file_name" --artwork REMOVE_ALL --overWrite > /dev/null 2>&1
+    
+    # Change album cover, song name, artist name, album name, year and Track number
     AtomicParsley "$song_file_name" --artwork "$album_name.jpg" --title "$name" --artist "$artist_name" --album "$album_name" --year $year --tracknum $index --overWrite > /dev/null 2>&1
+    
+    # Rename the downloaded file
     mv "$song_file_name" "$name.m4a"
+    
+    # Print output in console
     echo "🗹 ($index). $name"
+    
     cd ~
-    # exit
+    
     
 done < "$filename"
 
 
-    echo "----------------------------------------"
-    echo "➤ Download complete!"
+echo "----------------------------------------"
+echo "➤ Download complete!"
